a0147863710ee1d98fca167859633d00bd1763dc
d.argirov <argirov@outlook.com>
2018-04-14 19:10:29 +0300

    initial

---
+++ App/Program.cs
@@ -0,0 +1,27 @@
+﻿using Option;
+using System;
+using static Option.F;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var none = None;
+            var some = Some<int>(5);
+
+            Option<int> n = none;
+            Option<int> s = some;
+            Option<string> ss = None;
+            Option<string> sss = Some("dfsd");
+            Option<int> t = None;
+            Option<string> tt = null;
+
+            var t2 = t.Match(() => 6, x => x + 6);
+            Console.WriteLine(t2);
+
+            Console.WriteLine("sdfsd");
+        }
+    }
+}
---
+++ Funct/Option.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Option
+{
+    public static partial class F
+    {
+        public static Option.None None => Option.None.Default;
+        public static Option.Some<T> Some<T>(T value) => new Option.Some<T>(value);
+    }
+
+    public struct Option<T>
+    {
+        private T _value;
+        private bool _isSome;
+
+        private Option(T value)
+        {
+            _value = value;
+            _isSome = true;
+        }
+
+        public static implicit operator Option<T>(Option.None _) => new Option<T>();
+        public static implicit operator Option<T>(Option.Some<T> some) => new Option<T>(some.Value);
+        public static implicit operator Option<T>(T value) => value == null ? new Option<T>() : new Option<T>(value);
+
+        public R Match<R>(Func<R> none, Func<T, R> some)
+            => _isSome ? some(_value) : none();
+    }
+
+    namespace Option
+    {
+        public struct None
+        {
+            internal static readonly None Default = new None();
+        }
+
+        public struct Some<T>
+        {
+            internal T Value { get; }
+
+            internal Some(T value)
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                Value = value;
+            }
+        }
+    }
+}
